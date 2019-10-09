1973b964ce14c6b12b2c66d580e797b3bed59dd9
Jonah <jomartinez@univistainsurance.com>
2019-10-09 17:54:54 -0400

    Learning lambda expression

--- WorkingWithLINQ/Program.cs
+++ WorkingWithLINQ/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Linq;
 
 namespace WorkingWithLINQ
 {
@@ -6,7 +9,58 @@ namespace WorkingWithLINQ
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = @"C:\windows";
+            //ShowLargeFilesWithoutLinq(path);
+            Console.WriteLine("*****");
+            ShowLargeFilesWithLinq(path);
+        }
+
+        private static void ShowLargeFilesWithLinq(string path)
+        {
+            // Two versions of LINQ 
+
+            var query = from file in new DirectoryInfo(path).GetFiles()
+                        orderby file.Length descending
+                        select file;
+            foreach (var file in query.Take(5))
+            {
+                Console.WriteLine($"{file.Name,-20} : {file.Length,10:N0}");
+            }
+
+            Console.WriteLine("*****");
+
+
+            var query2 = new DirectoryInfo(path).GetFiles()
+                        .OrderByDescending(f => f.Length)
+                        .Take(5);
+
+            foreach (var file in query2)
+            {
+                Console.WriteLine($"{file.Name, -20} : {file.Length, 10:N0}");
+            }
+
+        }
+
+        private static void ShowLargeFilesWithoutLinq(string path)
+        {
+            DirectoryInfo directory = new DirectoryInfo(path);
+
+            FileInfo[] files = directory.GetFiles();
+            Array.Sort(files);
+            foreach (FileInfo file in files)
+            {
+                Console.WriteLine($"{file.Name} : {file.Length}");
+            }
         }
     }
+
+    //public class FileInfoComparer : IComparable<FileInfo>
+    //{
+    //    public int Compare(FileInfo x, FileInfo y)
+    //    {
+    //        return y.Length.CompareTo(x.Length);
+    //    }
+
+    //}
+
 }
